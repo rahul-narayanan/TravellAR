1940e51d15220b75fcee5eb3205a97dca61c4c93
Vatsal Modh <modhvatsal@gmail.com>
2021-11-16 23:18:19 -0500

    removed bg

--- src/core/theme.js
+++ src/core/theme.js
@@ -5,7 +5,7 @@ export const theme = {
   colors: {
     ...DefaultTheme.colors,
     text: '#000000',
-    primary: '#14B890',
+    primary: '#1590A1',
     secondary: '#14B890',
     error: '#f13a59',
   },
