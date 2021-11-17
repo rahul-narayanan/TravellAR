a74b6fa164dee24bedde9922022ef0d8b548cdc4
Rahul Narayanan <rahulnarayanan@zohomail.com>
2021-11-16 23:18:49 -0500

    Initial page changes

--- js/App.js
+++ js/App.js
@@ -1,57 +1,72 @@
-"use strict";
-/*
-Page contains Scan button. Button click navigates to HomePage
-*/
-import React, { Component } from "react";
-import { View, Button, StyleSheet, ImageBackground } from "react-native";
+import BG from "./res/app-bg-low.gif";
+
+import React, { useState, useRef, useEffect } from "react";
+import { View, StyleSheet, ImageBackground, Animated } from "react-native";
 import { ViroARSceneNavigator } from "react-viro";
 import HomePage from "./HomePage";
+import { Button } from "react-native-elements";
 
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { showScan: false };
-  }
-
-  render() {
-    if (this.state.showScan) {
-      return <ViroARSceneNavigator initialScene={{ scene: HomePage }} />;
-    }
-
-    return (
-      <ImageBackground
-        source={{
-          uri: "https://res.litfad.com/site/img/item/2020/09/14/1758200/584x584.jpg",
-        }}
-        style={{ flex: 1 }}
-      >
-        <View style={styles.container}>
-          <Button
-            title="SCAN QR CODE"
-            onPress={() => {
-              this.setState({ showScan: true });
-            }}
-          />
-        </View>
-      </ImageBackground>
-    );
-  }
-}
-
-//styles given to button
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
+    top: 100,
   },
-
-  text: {
-    margin: 30,
-    textAlign: "left",
-    color: "blue",
-    fontStyle: "italic",
-    fontFamily: "Calibri",
-    fontSize: 28,
+  button: {
+    backgroundColor: "grey",
+    borderColor: "white",
+    borderWidth: 1,
+    borderRadius: 10,
+    fontSize: 10,
   },
 });
+
+const App = () => {
+  const [showScan, setShowScan] = useState(false);
+
+  const fadeAnim = useRef(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    setTimeout(() => {
+      Animated.timing(fadeAnim, {
+        toValue: 1,
+        duration: 1500,
+      }).start();
+    }, 1300);
+  }, [fadeAnim]);
+
+  if (showScan) {
+    return <ViroARSceneNavigator initialScene={{ scene: HomePage }} />;
+  }
+
+  return (
+    <ImageBackground
+      // source={{
+      //   uri: "https://res.litfad.com/site/img/item/2020/09/14/1758200/584x584.jpg",
+      // }}
+      source={BG}
+      style={{ flex: 1 }}
+    >
+      <View style={styles.container}>
+        <Animated.View
+          style={[
+            styles.fadingContainer,
+            {
+              // Bind opacity to animated value
+              opacity: fadeAnim,
+            },
+          ]}
+        >
+          <Button
+            title="SCAN QR CODE"
+            onPress={() => setShowScan(true)}
+            buttonStyle={styles.button}
+          />
+        </Animated.View>
+      </View>
+    </ImageBackground>
+  );
+};
+
+export default App;
